6403872e572bdb8927cc4eee0dd1730cba3b9ed1
SeyemAhamed <seyamjh@gmail.com>
2024-01-21 13:24:52 +0600

    Nested for loop with 2D array

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// let human = {name: 'Bablu', age: 20, balance: 300, address: 'Dhaka', hobby: 'Singing'};
+
+// for(let key in human){
+//     console.log(human[key]); //human[name] //human[age] //human[balance]
+// }
+
+let twoDimensionalArray = [
+    ['Rahim', 'Karim', 'Bablu', 'Abdul Jobbar'],
+    ['Rubel', 'Shahin', 'Arif', 'Seyam'],
+    ['Utpol', 'Manik', 'Maruf', 'Mou'],
+ ]
+
+ for(let i=0; i<=2; i++){
+    for(let j=0; j<=3; j++){
+        console.log(twoDimensionalArray[i][j]);
+    }
+ }
+
+//  0 0
+//  0 1
+//  0 2
+//  0 3
+
+//  1 0
+//  1 1
+//  1 2
+//  1 3
+
+//  console.log(twoDimensionalArray[0][0]);
+//  console.log(twoDimensionalArray[0][1]);
+//  console.log(twoDimensionalArray[0][2]);
+//  console.log(twoDimensionalArray[0][3]);
+
+//  console.log(twoDimensionalArray[1][0]);
+//  console.log(twoDimensionalArray[1][1]);
+//  console.log(twoDimensionalArray[1][2]);
+//  console.log(twoDimensionalArray[1][3]);
+
+//  console.log(twoDimensionalArray[2][0]);
+//  console.log(twoDimensionalArray[2][1]);
+//  console.log(twoDimensionalArray[2][2]);
+//  console.log(twoDimensionalArray[2][3]);
+
